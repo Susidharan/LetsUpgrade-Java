ed2f699ff26a9acab591c4186ee8fcbe7ed9d3cf
Susidharan <70463708+Susidharan@users.noreply.github.com>
2020-10-10 21:40:56 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,13 @@
+package com.Susidharan;
+
+public class Employee {
+    String name = new String("The Name is Susidharan.");
+    String age = new String("The Age is 23.");
+    String city = new String("The City is Thanjavur.");
+
+    public void display() {
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(city);
+    }
+}
---
+++ Main.java
@@ -0,0 +1,13 @@
+package com.Susidharan;
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+         Employee employee=new Employee();
+         employee.display();
+
+
+    }
+}
